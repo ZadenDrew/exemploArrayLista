a76d0e9a3a9f8cac8466169f0fe78c7b58f45756
acabezaslopez <acabezaslopez@victoria11.danielcastelao.org>
2018-02-01 13:54:08 +0100

    EXEMPLOS

---
+++ src/exemploarraylista/ExemploArrayLista.java
@@ -0,0 +1,70 @@
+package exemploarraylista;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author acabezaslopez
+ */
+public class ExemploArrayLista {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*ArrayList lista = new ArrayList();
+        lista.add(1);
+        lista.add("aaa");
+        lista.add(1, 6);
+        lista.add("d");
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println(lista.get(i));
+        }*/
+        ArrayList<Integer> listaNum = new ArrayList();
+        int op;
+        Metodos obx = new Metodos();
+        do {
+            System.out.println("***********-MENÚ-***********\n1)Añadir elemento a la lista\n2)Visualizar\n3)Salir"
+                    + "\n4)Borrar posición\n5)Borrar elemento\n6)Mudar valor posición"
+                    + "\n7)Mudar valor\n8)Mayor o Menor?\n9)Par o Impar?");
+            op = Integer.parseInt(JOptionPane.showInputDialog("Elige una opción :"));
+            switch (op) {
+                case 1:
+                    obx.engadir(listaNum);
+                    break;
+                case 2:
+                    obx.amosarIterador(listaNum);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    obx.borrarElemento(listaNum);
+                    break;
+                case 5:
+                    obx.borrarElementoPosicion(listaNum, op);
+                    break;
+                case 6:
+                    obx.mudarValorPosicion(listaNum);
+                    break;
+                case 7:
+                    obx.mudarValor(listaNum, op);
+                    break;
+
+                case 8:
+                    obx.mayorMenor(listaNum);
+                    System.out.println("El mayor numero es :" + obx.getMayor() + "\nEl menor número es :" + obx.getMenor());
+                    break;
+
+                case 9:
+                    ArrayList<Integer> listaPar = new ArrayList();
+                    listaPar = obx.pares(listaNum);
+                    obx.amosarIterador(listaPar);
+                    break;
+            }
+        } while (op != 3);
+       // System.out.println("***********-MENÚ-***********\n1)Añadir elemento a la lista\n2)Visualizar\n3)Salir\n4)Borrar posición\n5)Borrar elemento\n6)Mudar valor posición\n7)Mudar valor");
+        //op = Integer.parseInt(JOptionPane.showInputDialog("Elige una opción :"));
+
+    }
+}
---
+++ src/exemploarraylista/Metodos.java
@@ -0,0 +1,97 @@
+package exemploarraylista;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author acabezaslopez
+ */
+public class Metodos {
+
+    private int mayor;
+    private int menor;
+
+    public int getMayor() {
+        return mayor;
+    }
+
+    public void setMayor(int mayor) {
+        this.mayor = mayor;
+    }
+
+    public int getMenor() {
+        return menor;
+    }
+
+    public void setMenor(int menor) {
+        this.menor = menor;
+    }
+
+    //añadir-elemento
+    public void engadir(ArrayList<Integer> lista) {
+        Integer n;
+        n = Integer.parseInt(JOptionPane.showInputDialog("numero"));
+        lista.add(n);
+
+    }
+    //mostrar-array
+    public void amosarIterador(ArrayList<Integer> lista) {
+        Iterator it = lista.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+    }
+
+    //borrar elemento-posicion
+    public void borrarElementoPosicion(ArrayList<Integer> lista, int posicion) {
+        lista.remove(posicion - 1);
+    }
+
+    //borrar-elemento
+    public void borrarElemento(ArrayList<Integer> lista) {
+        int elemento = Integer.parseInt(JOptionPane.showInputDialog("¿Qué elemento deseas borrar?"));
+        lista.remove(new Integer(elemento));
+
+    }
+
+    //cambiar valor-posicion
+    public void mudarValorPosicion(ArrayList<Integer> lista) {
+        int posicion = Integer.parseInt(JOptionPane.showInputDialog("¿Qué posición deseas modificar?"));
+        int valor = Integer.parseInt(JOptionPane.showInputDialog("¿Cal é o novo valor?"));
+        lista.set(posicion, valor);
+    }
+
+    //cambiar valor 
+    public void mudarValor(ArrayList<Integer> lista, int elemento) {
+        int valorAnt = Integer.parseInt(JOptionPane.showInputDialog("¿Qué elemento deseas modificar?:"));
+        //   int valorNovo=Integer.parseInt(JOptionPane.showInputDialog("¿Qué elemento deseas modificar?:"));
+        int valorNovo = lista.indexOf(elemento);
+        lista.add(valorAnt, valorNovo);
+    }
+
+    //mostrar el mayor y el menor
+    public void mayorMenor(ArrayList<Integer> lista) {
+        Comparator<? super Integer> c = null;
+        lista.sort(c);
+        mayor = lista.get(lista.size() - 1);
+        menor = lista.get(0);
+    }
+
+    //mostrar los pares de la lista
+    public ArrayList<Integer> pares(ArrayList<Integer> lista) {
+        Iterator it = lista.iterator();
+        ArrayList<Integer> listaPar = new ArrayList();
+        while (it.hasNext()) {
+            int num = (int) it.next();
+            if (num % 2 == 0) {
+                listaPar.add(num);
+            }
+         //   System.out.println(it.next());
+        }
+        return listaPar;
+    }
+}
